d48b9d33d54b7f65953bb7c8421acdaa425965a3
Felipe Santos Ferreira <minuguaxi@gmail.com>
2024-09-16 20:04:39 -0300

    Update modelo.go

--- modelos/modelo.go
+++ modelos/modelo.go
@@ -22,7 +22,7 @@ func Inseri(nome_paciente, tipo_plano string, telefone_paciente int, cpf int, em
 		panic(err.Error())
 	}
 
-	inserir.Exec(nome, nomedamae, nomedopai, telefone, cpf, email, senha)
+	inserir.Exec(nome_paciente, tipo_plano, telefone_paciente, cpf, email, senha)
 	defer db.Close()
 }
 
@@ -38,25 +38,23 @@ func Buscar() []Login {
 	logar := []Login{}
 
 	for selectReg.Next() {
-		var id int
-		var nome string
-		var nomedamae string
-		var nomedopai string
-		var telefone int
+		var id_paciente int
+		var nome_paciente string
+		var tipo_plano string
+		var telefone_paciente int
 		var cpf int
 		var email string
 		var senha int
 
-		err = selectReg.Scan(&id, &nome, &nomedamae, &nomedopai, &telefone, &cpf, &email, &senha)
+		err = selectReg.Scan(&id_paciente, &nome_paciente, &tipo_plano, &telefone_paciente, &cpf, &email, &senha)
 		if err != nil {
 			panic(err.Error())
 		}
 
-		l.Id = id
-		l.Nome = nome
-		l.Nomedamae = nomedamae
-		l.Nomedopai = nomedopai
-		l.Telefone = telefone
+		l.Id_paciente = id_paciente
+		l.Nome_paciente = nome_paciente
+		l.Tipo_plano = tipo_plano
+		l.Telefone_paciente = telefone_paciente
 		l.CPF = cpf
 		l.Email = email
 		l.Senha = senha
@@ -69,23 +67,23 @@ func Buscar() []Login {
 
 }
 
-func Deleta(id string) {
+func Deleta(id_paciente string) {
 	db := db.Acesse()
 
-	deletar, err := db.Prepare("delete from login where id=$1")
+	deletar, err := db.Prepare("delete from login where id_paciente=$1")
 	if err != nil {
 		panic(err.Error())
 	}
 
-	deletar.Exec(id)
+	deletar.Exec(id_paciente)
 	defer db.Close()
 
 }
 
-func Editar(id string) Login {
+func Editar(id_paciente string) Login {
 	db := db.Acesse()
 
-	atualizar, err := db.Query("select * from login where id = $1", id)
+	atualizar, err := db.Query("select * from login where id_paciente = $1", id_paciente)
 	if err != nil {
 		panic(err.Error())
 	}
@@ -93,20 +91,19 @@ func Editar(id string) Login {
 	Atualize := Login{}
 
 	for atualizar.Next() {
-		var id int
-		var nome, nomedamae, nomedopai string
-		var telefone, cpf int
+		var id_paciente int
+		var nome_paciente, tipo_plano string
+		var telefone_paciente, cpf int
 		var email string
 		var senha int
-		err = atualizar.Scan(&id, &nome, &nomedamae, &nomedopai, &telefone, &cpf, &email, &senha)
+		err = atualizar.Scan(&id_paciente, &nome_paciente, &telefone_paciente, &tipo_plano, &cpf, &email, &senha)
 		if err != nil {
 			panic(err.Error())
 		}
-		Atualize.Id = id
-		Atualize.Nome = nome
-		Atualize.Nomedamae = nomedamae
-		Atualize.Nomedopai = nomedopai
-		Atualize.Telefone = telefone
+		Atualize.Id_paciente = id_paciente
+		Atualize.Nome_paciente = nome_paciente
+		Atualize.Tipo_plano = tipo_plano
+		Atualize.Telefone_paciente = telefone_paciente
 		Atualize.CPF = cpf
 		Atualize.Email = email
 		Atualize.Senha = senha
@@ -116,13 +113,13 @@ func Editar(id string) Login {
 	return Atualize
 }
 
-func Atualizar(id int, nome, nomedamae, nomedopai string, telefone, cpf int, email string, senha int) {
+func Atualizar(id_paciente int, nome_paciente, tipo_plano string, telefone_paciente, cpf int, email string, senha int) {
 	db := db.Acesse()
 
-	atualiza, err := db.Prepare("update login set nome=$1, nomedamae=$2, nomedopai=$3, telefone=$4, cpf=$5, email=$6, senha=$7 where id=$8")
+	atualiza, err := db.Prepare("update login set nome_paciente=$1, tipo_plano=$2, telefone_paciente=$3, cpf=$4, cpf=$5, email=$6, senha=$7 where id_paciente=$8")
 	if err != nil {
 		panic(err.Error())
 	}
-	atualiza.Exec(nome, nomedamae, nomedopai, telefone, cpf, email, senha, id)
+	atualiza.Exec(nome_paciente, tipo_plano, telefone_paciente, cpf, email, senha, id_paciente)
 	defer db.Close()
 }
